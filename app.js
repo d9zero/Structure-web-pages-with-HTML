1ac1b94e2c6243255559bdf265ab3282cb2e9dfb
d9zero <augurson90@gmail.com>
2020-07-22 14:59:20 -0700

    new js file

---
+++ app.js
@@ -0,0 +1,12 @@
+
+var userName = prompt('What is your name?');
+
+
+
+alert('Hello There....' + userName);
+
+document.write('<hs>') + userName + ''
+
+
+
+confirm('Are you ready to rock?');
---
+++ time.js
@@ -0,0 +1,11 @@
+var userTime = prompt('What hour is it in military time 0-24?');
+
+
+
+if(userTime <= 12){
+    //if the time is <12., prompt good morning
+} else if (userTime > 12 && userTime <= 18) {
+    // if the time is > 12 and < 18 prompt good afternoon
+} else {
+    // if the time is > 18 then prompt good evening
+}
