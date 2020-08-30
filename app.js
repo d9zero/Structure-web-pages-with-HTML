2060a04d03ed1a6e9db9bd459f48664838f09c3a
d9zero <augurson90@gmail.com>
2020-08-30 09:56:12 -0700

    review

--- app.js
+++ app.js
@@ -9,3 +9,21 @@ document.write('<hs>') + userName + ''
 
 
 
+function getuserTime(){
+  var userTimeFN = prompt('Do You Know What Time it is!?');
+  alert('Well You Know what that means!' + userTimeFN + 'Its Time to Rock!');
+  document.write('<hs2>' + userTimeFN + '</h2>');
+  return userTimeFN;
+}
+
+var usertime = getuserTime();
+
+
+
+  if(userTime <= 12){
+      alert('Good Morning');
+  } else if (userTime >= 12 && userTime < 18) {
+      // if the time is > 12 and < 18 prompt good afternoon
+  } else {
+      // if the time is > 18 then prompt good evening
+  }
